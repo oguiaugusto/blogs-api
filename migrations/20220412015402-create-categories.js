1119247d02daa446afdd66ea6a4837bcd17cc2e5
Guilherme <guilhaugusto05@gmail.com>
2022-04-11 23:03:09 -0300

    feat: create Categories table

---
+++ migrations/20220412015402-create-categories.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('Categories');
+  }
+};
